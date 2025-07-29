fb3b45c690fd6e824a4f7bdc6c91c009399571a1
Corneliu Procopenco <procorneliu@gmail.com>
2025-07-29 18:59:05 +0300

    app dockerized

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:alpine
+
+WORKDIR /app
+
+COPY package.json package-lock.json ./
+
+RUN npm ci
+
+COPY . ./
+
+# ENV PORT=3000
+
+EXPOSE 3000
+
+CMD [ "npm", "start" ]
--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import './utils/dotenv.ts';
 import './utils/db.ts';
 import app from './app.ts';
 
-const server = app.listen(process.env.PORT || 3000, () => {
+const server = app.listen(Number(process.env.PORT) || 3000, '0.0.0.0', () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
 
